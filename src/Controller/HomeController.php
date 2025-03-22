40dfa8193ba991f4e6127642b538fbae42b93973
Samuel Staf <samuelstaf07@gmail.com>
2025-03-22 16:07:33 +0100

    add news section and footer. Old controller removed

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Rentals;
 use App\Repository\CommentsRepository;
+use App\Repository\PostsRepository;
 use App\Repository\RentalsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +13,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(RentalsRepository $rentalsRepository, CommentsRepository $commentsRepository): Response
+    public function index(RentalsRepository $rentalsRepository, PostsRepository $postsRepository): Response
     {
         $rentals = $rentalsRepository->findAllRentalsWithDiscount();
+        $posts = $postsRepository->findLatestActivePosts();
 
         return $this->render('home/index.html.twig', [
             'rentals' => $rentals,
+            'posts' => $posts,
         ]);
     }
 }
--- src/Repository/PostsRepository.php
+++ src/Repository/PostsRepository.php
@@ -16,28 +16,15 @@ class PostsRepository extends ServiceEntityRepository
         parent::__construct($registry, Posts::class);
     }
 
-    //    /**
-    //     * @return Posts[] Returns an array of Posts objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findLatestActivePosts(): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.is_active = :isActive')
+            ->setParameter('isActive', true)
+            ->orderBy('p.created_at', 'DESC')
+            ->setMaxResults(5)
+            ->getQuery()
+            ->getResult();
+    }
 
-    //    public function findOneBySomeField($value): ?Posts
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
